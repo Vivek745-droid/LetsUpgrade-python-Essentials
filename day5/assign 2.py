50ed0b19ea87cd5bb5fac005fe3571e8fa5eb70e
Vivek745-droid <70594623+Vivek745-droid@users.noreply.github.com>
2020-09-11 23:45:57 +0530

    Delete assign 2.py

--- day5/assign 2.py
+++
@@ -1,9 +0,0 @@
-def isPrime(x):
-    for n in range(2,x):
-        if x%n==0:
-            return False
-        else:
-            return True
-
-fltrObj=filter(isPrime, range(2500))
-print ('Prime numbers between 1-10:', list(fltrObj))
